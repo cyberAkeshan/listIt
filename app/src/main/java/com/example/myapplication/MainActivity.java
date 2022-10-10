fddd245d19ae215a61eee6db7fab08bc264ae600
KadrioL <adrian.69518@web.de>
2022-10-10 18:37:59 +0200

    Added onClickListener for addListButton, tested it with a small toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addListButton;
+    SharedPreferences sp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListButton = findViewById(R.id.addFab);
+
+        addListButton.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show();
+        });
     }
 }
